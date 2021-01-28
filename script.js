f43b77a1c843309ea9a02c88c92a63b9a7f22a34
César Higashi <76760288+higashi04@users.noreply.github.com>
2021-01-28 15:40:05 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class Calculator {
     clear() {
       this.lotSize = ''
       this.sampleSize = ''
+      lotQty.innerText = ''
       this.operation = undefined
     }
   
@@ -20,30 +21,64 @@ class Calculator {
     }
   
     chooseAql(aql) {
-      if (this.sampleSize === '') return
-      if (this.lotSize !== '') {
-        this.compute()
-      }
+     if (this.sampleSize === '') return
+     if (this.lotSize !== '') {
+       this.compute()
+     }
       this.aql = aql
       this.sampleSize = this.lotSize
       this.lotSize = ''
     }
   
     compute() {
-      let computation
-      parseFloat(this.sampleSize)
-     // const lot = parseFloat(this.lotSize)
-     // if (isNaN(sample)||isNaN(lot)) return
-     switch (this.aql){
-     case 0.010:
-      if (this.sampleSize > 2 || this.sampleSize < 1200)
-          { //this.lotQty.innerText = 'all'
-          let samples = document.createElement(div);
-          samples.innerText = 'All'
-          lotQty.append('All');
-        };
+     const sample = parseFloat(this.sampleSize)
+     const lot = parseFloat(this.lotSize)
+     if (aqlSelector.value == 0.010) {
+       if (this.sampleSize > 0 || this.sampleSize <= 1200)
+             {lotQty.innerText = 'All'} 
+             else if (this.sampleSize >= 1201) 
+             {lotQty.innerText = '1250'};
+      } else if (aqlSelector.value == 0.015) {
+        if (this.sampleSize >= 2 || this.sampleSize <= 500)
+             {lotQty.innerText = 'All'} 
+             else if(this.sampleSize >= 501 || this.sampleSize <= 5000000)
+             {lotQty.innerText = 800} 
+             else if (this.sampleSize >= 500001) 
+             {lotQty.innerText = 1200}
+      } else if (aqlSelector.value == 0.025) {
+        if (this.sampleSize > 2 || this.sampleSize <= 500)
+             {lotQty.innerText = 'All'}
+             else if (this.sampleSize >= 501 || this.sampleSize <= 150000)
+             {lotQty.innerText = 500}
+             else if (this.sampleSize >= 150001 || this.sampleSize <= 500000)
+             {lotQty.innerText = 750} else if 
+             (this.sampleSize >= 500001) 
+             {lotQty.innerText = 1112}
 
-    }
+      } else if (aqlSelector.value == 0.040){
+        if (this.sampleSize >= 2 || this.sampleSize <= 280)
+        {lotQty.innerText = 'All'} else if 
+        (this.sampleSize >= 281 || this.sampleSize <= 35000)
+        {lotQty.innerText = 315} else if 
+        (this.sampleSize >= 35001 || this.sampleSize <= 150000) 
+        {lotQty.innerText = 490} else if 
+        (this.sampleSize >= 150001)
+        {lotQty.innerText = 715}
+      }
+      else if (aqlSelector.value == 0.065){ 
+        if (this.sampleSize >= 2 || this.sampleSize <= 150)
+        {lotQty.innerText = 'All'} else if 
+        (this.sampleSize >= 151 || this.sampleSize <= 10000)
+        {lotQty.innerText = '200'} else if 
+        (this.sampleSize >= 10001 || this.sampleSize <= 35000)
+        {lotQty.innerText = '300'} else if 
+        (this.sampleSize >= 35001 || this.sampleSize <= 500000)
+        {lotQty.innerText = '476'} else if 
+        (this.sampleSize >= 500001)
+        {lotQty.innerText = '556'}
+      }
+     
+     
     
       
       };
@@ -84,6 +119,7 @@ const lotQty = document.querySelector('.user-input');
 const sampleDisplay = document.querySelector('.sample-size')
 const result = document.querySelector('.submit');
 const deleteBtn = document.querySelector('.delete');
+//const span = document.querySelector('.inspect')
 
 const calculator = new Calculator(lotQty, sampleDisplay)
 
@@ -94,7 +130,7 @@ numberBtns.forEach(button => {
     })
 })
 
-aqlSelector.addEventListener('change', select =>{
+aqlSelector.addEventListener('input', select =>{
   calculator.chooseAql(select.value)
   calculator.updateDisplay()
 })
@@ -111,6 +147,8 @@ allClearBtn.addEventListener('click', button => {
   })
 
   result.addEventListener('click', button => {
+    lotQty.innerText = ''
     calculator.compute()
     calculator.updateDisplay()
+    
   })
